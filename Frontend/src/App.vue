a56436de09e3ac18d32edefa32fe8fe505e37c6f
rounakranjan-compro <rounak.ranjan@comprotechnologies.com>
2025-12-29 12:54:13 +0530

    Sidebar: Added updated App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,18 +1,19 @@
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import Sidebar from './components/Sidebar.vue';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <header>
+    <Navbar/>
+    </header>
+
+   <aside class="p-4 pt-24  overflow-y-auto z-40 ">
+        <Sidebar/>
+    </aside>
+    
 </template>
 
 <style scoped>
