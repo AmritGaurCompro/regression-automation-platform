0893ed56ac9c4038bf14d9ac2bf0d289307cef65
sarthakgoyal19 <sarthak.goyal@comprotechnologies.com>
2026-01-19 11:47:57 +0530

    build: setup controller and UI to support node agent

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :test_runs, only: [:create]
+    post 'test/:id', to: 'test_runs#create'
+  end
 end
+
