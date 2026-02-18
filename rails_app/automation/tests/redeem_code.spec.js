6fd5c36a51a9da6a4a436b94f439dad905fc0dd3
compro <amrit.gaur@comprotechnologies.com>
2026-02-18 11:46:13 +0530

    update: updated code 4.0

--- rails_app/automation/tests/redeem_code.spec.js
+++ rails_app/automation/tests/redeem_code.spec.js
@@ -7,5 +7,5 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('Compro11');
   await page.getByRole('button', { name: 'Log In' }).click();
-  await page.getByRole('link', { name: 'My Notes' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).click();
 });
