139843a16338aa62b76674affa4b85ca702b618d
sarthakgoyal19 <sarthak.goyal@comprotechnologies.com>
2026-02-24 15:55:23 +0530

    updated with deployed frontend url

--- rails_app/config/initializers/cors.rb
+++ rails_app/config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'localhost:3000', 'localhost:5173'
+    origins 'localhost:3001', 'localhost:3000', 'localhost:5173','https://regression-automation-platform.vercel.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete]
   end
 end
