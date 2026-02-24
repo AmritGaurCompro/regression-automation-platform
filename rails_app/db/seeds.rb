6ccc48892abd58213453999eaaf0c61f5ec75442
rounakranjan-compro <rounak.ranjan@comprotechnologies.com>
2026-02-24 15:01:00 +0530

    Add seed data for production

--- rails_app/db/seeds.rb
+++ rails_app/db/seeds.rb
@@ -1,7 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "🌱 Seeding database..."
+
+# Create sample tests
+Test.create!([
+  {
+    name: 'Login Test',
+    description: 'Tests user login functionality'
+  },
+  {
+    name: 'Navigation Test',
+    description: 'Tests navigation between pages'
+  },
+  {
+    name: 'Redeem Code Test',
+    description: 'Tests code redemption feature'
+  }
+])
+
+puts "✅ Created #{Test.count} tests"
+puts "🎉 Seeding completed!"
