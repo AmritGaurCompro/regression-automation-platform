9465593ab3edc05949475267649ab3106263fb8c
sarthakgoyal19 <sarthak.goyal@comprotechnologies.com>
2026-03-02 12:39:59 +0530

    changes for headless mode

--- rails_app/automation/playwright.config.js
+++ rails_app/automation/playwright.config.js
@@ -6,14 +6,29 @@ const retries = Number(process.env.PW_RETRIES);
 module.exports = defineConfig({
   testDir: './tests',
 
-  fullyParallel: true,
+  // 🔥 Disable parallel execution in container
+  fullyParallel: false,
+
+  // 🔥 Force single worker ALWAYS in deployment
+  workers: 1,
+
   forbidOnly: !!process.env.CI,
   retries: Number.isNaN(retries) ? 2 : retries,
-  workers: process.env.CI ? 1 : undefined,
 
   outputDir: `artifacts/run-${runId}`,
 
   use: {
+    headless: true,
+
+    // 🔥 REQUIRED FOR RENDER
+    launchOptions: {
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage'
+      ],
+    },
+
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
@@ -21,7 +36,7 @@ module.exports = defineConfig({
 
   reporter: [
     ['html'],
-     ['json', { outputFile: `artifacts/run-${runId}/result.json` }]
+    ['json', { outputFile: `artifacts/run-${runId}/result.json` }]
   ],
 
   projects: [
