c3b3b2fb474c57d9718910c3f1486bbc8eb951d6
sarthakgoyal19 <sarthak.goyal@comprotechnologies.com>
2026-03-02 14:21:00 +0530

    changes for headless

--- rails_app/automation/playwright.config.js
+++ rails_app/automation/playwright.config.js
@@ -3,17 +3,20 @@ const { defineConfig, devices } = require('@playwright/test');
 const runId = new Date().toISOString().replace(/[:.]/g, '-');
 const retries = Number(process.env.PW_RETRIES);
 
+
 module.exports = defineConfig({
   testDir: './tests',
 
   // 🔥 Disable parallel execution in container
   fullyParallel: false,
-
+  
   // 🔥 Force single worker ALWAYS in deployment
   workers: 1,
 
   forbidOnly: !!process.env.CI,
   retries: Number.isNaN(retries) ? 2 : retries,
+  timeout: 120000,
+  globalTimeout: 300000,  
 
   outputDir: `artifacts/run-${runId}`,
 
