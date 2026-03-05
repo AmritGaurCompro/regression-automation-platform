638008695e5a5ac2081bfe69854a9e591a41545a
AmritGaurCompro <163517547+AmritGaurCompro@users.noreply.github.com>
2026-03-05 15:16:39 +0530

    Enhance test_runs routes with artifacts endpoint
    Added a route for receiving artifacts in test runs.

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :record_tests, only: [:index,:create]
+    resources :record_tests, only: [:index, :create]
     resources :tests do
-      resources :test_runs, only: [:index, :create, :show]      
+      resources :test_runs, only: [:index, :create, :show] do
+        post 'artifacts', to: 'test_runs#receive_artifacts', on: :member
+      end
     end
   end
 end
