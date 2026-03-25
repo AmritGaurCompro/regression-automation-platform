2c96608ac4e39d19f402c242c07173f75f3c9344
compro <amrit.gaur@comprotechnologies.com>
2026-03-25 17:04:34 +0530

    update: added url for record

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_03_18_100229) do
+ActiveRecord::Schema.define(version: 2026_03_25_113138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2026_03_18_100229) do
     t.bigint "script_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "vnc_url"
     t.index ["script_id"], name: "index_tests_on_script_id"
   end
 
