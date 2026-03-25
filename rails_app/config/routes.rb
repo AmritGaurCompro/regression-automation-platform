c171af0de863805d097e12dea0182b81e8ee27e0
AmritGaurCompro <163517547+AmritGaurCompro@users.noreply.github.com>
2026-03-25 16:50:24 +0530

    Add routes for updating VNC URL in tests and test runs

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :record_tests, only: [:index, :create]
     resources :tests do
+      post 'vnc_url', to: 'tests#update_vnc_url', on: :member
       resources :test_runs, only: [:index, :create, :show] do
         post 'artifacts', to: 'test_runs#receive_artifacts', on: :member
         post 'vnc_url', to: 'test_runs#update_vnc_url', on: :member
